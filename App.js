c439af7233a42466b9e2400e88844857bfc4a3d5
Edward Apostol <ed@apostol.com>
2020-03-08 00:40:11 -0500

    [dev] created empty file Search.js. Updated instructions in App.js

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 /*
-* 1. Replace the text 'Open up App.js to start working on your app!' with 'React Native Creative - Spotify Player'
-* 2. Confirm that your changes are seen in the your iOS or Android Emulator.
-* 3. Create folders in your project called api and components.
-* 4. Commit your changes.
+* 5. Now that you have created a basic React Native Project, and modified some basic text, let's create a component.
+* 6. in the Components folder, create a new file, called Search.js (it's been created for you)
+* 7. Fulfill the steps described in Search.js
+* 8.
+* 9.
+* 10.
+*
 *
 *
 *
@@ -13,7 +16,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>React Native Creative - Spotify Player</Text>
     </View>
   );
 }
