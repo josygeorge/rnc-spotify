86bfc0855899fbde0652e695a7743fc2cb88591d
Edward Apostol <ed@apostol.com>
2020-03-08 00:10:34 -0500

    [dev] prep step 01-exploration

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+/*
+* 1. Replace the text 'Open up App.js to start working on your app!' with 'React Native Creative - Spotify Player'
+* 2. Confirm that your changes are seen in the your iOS or Android Emulator.
+* 3. Create folders in your project called api and components.
+* 4. Commit your changes.
+*
+*
+*
+* */
 export default function App() {
   return (
     <View style={styles.container}>
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # React Native Creative - Spotify Player - Work Files
 
+
+Branches:
+
+master - The original and final branch in the exercise
+00-baseline - initial setup and install of a react native project
+01-exploration - play around with the content, and re-arrange some code
+
+
+
